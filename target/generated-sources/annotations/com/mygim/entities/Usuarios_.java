0f1a54f574cd0819a8ff70fa45948c4875ffe0f8
Ataraxta <56114075+Ataraxta@users.noreply.github.com>
2020-05-20 16:08:48 +0200

    Error glassfish connector

--- target/generated-sources/annotations/com/mygim/entities/Usuarios_.java
+++ target/generated-sources/annotations/com/mygim/entities/Usuarios_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-19T19:26:43")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-20T16:03:27")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
