3170223c95b23a3e71afb8fff6e33ca227c2a108
sergio10 <sergio.diez.sergio.diez@gmail.com>
2020-05-23 16:15:57 +0200

    Fix de modificar Actividad

--- target/generated-sources/annotations/com/mygim/entities/Usuarios_.java
+++ target/generated-sources/annotations/com/mygim/entities/Usuarios_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-23T15:53:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-05-23T16:13:22")
 @StaticMetamodel(Usuarios.class)
 public class Usuarios_ { 
 
